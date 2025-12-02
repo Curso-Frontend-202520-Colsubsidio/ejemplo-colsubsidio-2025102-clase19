2bb16576e5aef5b99e52f8b97a1eefc7711bf946
Nicolas Saavedra <personal@nicolassaavedra.com>
2025-12-01 19:05:14 -0500

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-amber-100">
+        <div className="text-center">
+          <h1 className="text-6xl">Hola a todos desde Vercel</h1>
+          <p className="text-2xl">
+            Este es un proyecto para probar la plataforma y como hacer el
+            proceso de despliege
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
